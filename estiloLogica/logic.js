0e8e5d62b18afa17d1c183b1b78b6922ec3e760e
Jorge Orozco <yorchorozco@gmail.com>
2014-03-13 03:52:34 -0600

    insertRectangle
    metodo funcionando de insertaNuevoRegistro en la parte grafica usando un
    rectangulo

--- estiloLogica/logic.js
+++ estiloLogica/logic.js
@@ -1,4 +1,28 @@
-window.onload = function(){
+var misREctanguloas = new Array();
 
-	alert("hola mapa");
+function insertaNuevoRegistro(){
+
+	var centro = miMapa.getCenter();
+
+	var bounds = new google.maps.LatLngBounds(
+      centro,
+      new google.maps.LatLng(centro.lat()+0.0010, centro.lng()+0.0010)
+  	);
+
+	 var nuevoRect = new google.maps.Rectangle({
+	    bounds: bounds,
+	    editable: true,
+	    draggable: true,
+	    fillColor: "Lime",
+	    fillOpacity: 0.5,
+	    strokeColor: "LimeGreen",
+	    strokeOpacity: 1.0
+	  });
+
+	misREctanguloas.push(nuevoRect);
+
+	nuevoRect.setMap(miMapa);
+
+
+
 }
